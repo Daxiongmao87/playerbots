5e1844bc70b9c7736a8084ceac9088cb59ba2e17
daxiognmao87 <daxiognmao87@gmail.com>
2025-01-17 13:59:28 -0600

    more fixes

--- playerbot/strategy/actions/SayAction.cpp
+++ playerbot/strategy/actions/SayAction.cpp
@@ -573,10 +573,8 @@ void ChatReplyAction::ChatReplyDo(Player* bot, uint32 type, uint32 guid1, uint32
                 else {
                     if (sPlayerbotAIConfig.llmPersonalityGenerationEnabled)
                     {
-                        std::map<std::string, std::string> jsonFill;
                         sLog.outString("BotLLM: Generating personality for bot %s", bot->GetName());
-                        //std::string prompt = sPlayerbotAIConfig.llmPersonalityGenerationPrompt;
-                        std::string prompt;
+                        std::string prompt = sPlayerbotAIConfig.llmPersonalityGenerationPrompt;
                         for (auto& placeholder : placeholders)
                         {
                             prompt = boost::replace_all_copy(prompt, placeholder.first, placeholder.second);
@@ -588,9 +586,9 @@ void ChatReplyAction::ChatReplyDo(Player* bot, uint32 type, uint32 guid1, uint32
 
                         if (!randomSeeds.empty())
                         {
-                          prompt += "\nTreat these terms as random generation seeds:" + randomSeeds;
+                          prompt += "\\n\\nTreat these terms as random generation seeds:" + randomSeeds;
                         }
-                        prompt += "\nReturn only the generated personality. Limit output to 100 tokens.";
+                        prompt += "\\nReturn only the generated personality. Limit output to 100 tokens. Personality: ";
 
                         std::vector<std::string> debugLines; // or fill with some debug info
 
